51863f4f239d0d00a11d5e483f0dac450c5cf987
David0225 <learn0225@126.com>
2018-03-28 14:35:11 +0800

    V2

--- log/pgtosqlserver.py
+++ log/pgtosqlserver.py
@@ -7,46 +7,39 @@ Created on Tue Mar 27 17:59:22 2018
 
 #import psycopg2
 from sqlalchemy import create_engine
-#import pymssql
+import pymssql
 import pandas as pd
-import io
+#import io
+import numpy as np
+import time
 
-#方法一：使用Pandas库的to_sql方法，但是效率太差
+#方法一：使用Pandas库的to_sql方法，但是效率太差(一个字段，8分钟46万)
 def sqlserverinsert(data):
-    
-#    conn=pymssql.connect(host='10.99.66.46',user='david',password='life@1234',database='LifeVc.com',charset='GBK')
-#    conn=pymssql.connect(host=ip,user=user,password=password,database=database,charset=charset)
+
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
     engine = create_engine('mssql+pymssql://david:life@1234@10.99.66.46/LifeVc.com')
     
     data.to_sql('logcheck', engine, if_exists='append',index=False, chunksize=10000)
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
     
-
-def outsql(data_Out):
-    #把Dataframe导入sqlserver数据库
-    engine = create_engine('mssql+pymssql://david:life@1234@10.99.66.46/LifeVc.com') #create_engine说明：dialect[+driver]://user:password@host/dbname[?key=value..]
-#    print(data_Out)
+#方法二：直接通过语句导入，效率和Pandas差不多，而且语句比较复杂
+def sqlserverinsertV2(data):
+    #Dataframe转化为listoftuple
+    train_data = np.array(data)
+    train_data_list = train_data.tolist()
     
-#    print('test3333')
-    #创建一个内存文件
-    output = io.StringIO()
+    train_data_list_tuple=[]
+    for l in train_data_list:
+        train_tuple=tuple((l))
+        train_data_list_tuple.append(train_tuple)
     
-#    print(output.readline())
+    #连接sqlserver，并导入表
+    conn=pymssql.connect(host='10.99.66.46',user='david',password='life@1234',database='LifeVc.com',charset='GBK')
+    cur=conn.cursor()
     
-    # ignore the index  
-    data_Out.to_csv(output, sep='\t',index = False, header = False)  
-    output.getvalue()
-    # jump to start of stream  
-    output.seek(0)
-      
-    connection = engine.raw_connection() #engine 是 from sqlalchemy import create_engine  
-    cursor = connection.cursor()
-    # null value become ''
-#    print('test1111')
-    cursor.copy_from(output,'logcheck',null='',sep='\t')
-#    print('test1112')
-    connection.commit()
-    cursor.close()
-
+    cur.executemany("insert into logcheck(tracerid,spanid) values (%s,%s)",train_data_list_tuple)
+    
+    conn.commit()
 
 def pgselect():
     engine = create_engine('postgresql://postgres:123456@10.99.66.86:5432/postgres') #create_engine说明：dialect[+driver]://user:password@host/dbname[?key=value..]
@@ -60,4 +53,4 @@ data = pgselect()
 
 sqlserverinsert(data)
 
-outsql(data)
+sqlserverinsertV2(data)
